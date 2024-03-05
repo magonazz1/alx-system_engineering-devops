36296f067e2104f02d7f1d10faae75d081f10057
magonazz1 <martinmagonazz@outlook.com>
2024-03-05 14:31:06 -0800

    api

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -7,7 +7,7 @@ def recurse(subreddit, hot_list=[], after="", count=0):
     """Returns a list of titles of all hot posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0"
+        "User-Agent": "0x16-api_advanced:v1.0.0 (by /u/magonazz1"
     }
     params = {
         "after": after,
